5cd77762fbd67f695a6af53336146a297d60ed1f
jayden <jayden@verizonmedia.com>
2022-02-23 10:38:34 +0800

    update pkgs

--- src/extension.ts
+++ src/extension.ts
@@ -2,63 +2,92 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+
 var path = require('path');
 let LineByLine = require('n-readlines');
-var fileGrep = require('./grep');
+var split = require('split')
+    , EventEmitter = require('events').EventEmitter;
+
+var fileGrep = function (stream, reg) {
+    var lineNumber = 0,
+        found = 0,
+        e = new EventEmitter(),
+        regExp = new RegExp(reg);
+    stream
+        .pipe(split())
+        .on('data', function (line) {
+            lineNumber += 1
+            var foundItems = regExp.exec(line);
+            if (foundItems && foundItems.length > 0 && foundItems[0] !== '') {
+                var item = foundItems[0];//Only use the first found
+                e.emit('found', item, lineNumber);
+                found += 1;
+            }
+            ;
+
+        })
+        .on('end', function () {
+            e.emit('end', found);
+        })
+        .on('error', function (error) {
+            e.emit('error', error);
+        })
+    return e;
+};
 var fs = require('fs');
 var STATE_KEY = "ctagsSupport";
-var navigationHistory = [];    
+var navigationHistory = [];
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    
+
     //restore previous history   
-    restoreWorkspaceState(context,STATE_KEY,(val)=>{
-        try{
+    restoreWorkspaceState(context, STATE_KEY, (val) => {
+        try {
             let savedState = JSON.parse(val);
-            if(savedState.navigationHistory){
+            if (savedState.navigationHistory) {
                 navigationHistory = JSON.parse(val).navigationHistory;
             }
-        }catch(e){
+        } catch (e) {
             console.log(e);
-        }     
+        }
     });
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "ctags-support" is now active!');
-    
+
     // The commandId parameter must match the command field in package.json
-    let disposableFindTags = vscode.commands.registerCommand('extension.searchCTags', () => {         
-         console.log("Read .tag file from:" + path.join(vscode.workspace.rootPath,'.tags'));
-         let tags = loadTags(path.join(vscode.workspace.rootPath,'.tags'));
-         searchTags(context, tags);
+    let disposableFindTags = vscode.commands.registerCommand('extension.searchCTags', () => {
+        console.log("Read .tag file from:" + path.join(vscode.workspace.rootPath, '.tags'));
+        let tags = loadTags(path.join(vscode.workspace.rootPath, '.tags'));
+        searchTags(context, tags);
     });
 
     let disposableShowNavigationHistory = vscode.commands.registerCommand('extension.showNavigationHistory', () => {
-       
-        vscode.window.showQuickPick(navigationHistory).then(val=> {
-            navigateToDefinition(val.filePath,val.pattern);
+
+        vscode.window.showQuickPick(navigationHistory).then(val => {
+            navigateToDefinition(val.filePath, val.pattern);
         });
-  
+
     });
 
     let disposableClearAllNavigationHistory = vscode.commands.registerCommand('extension.clearAllNavigationHistory', () => {
-       
+
         navigationHistory = [];
-  
+
     });
 
     let disposableClearOneNavigationHistory = vscode.commands.registerCommand('extension.clearOneNavigationHistory', () => {
-       
-        vscode.window.showQuickPick(navigationHistory).then(val=> {
-            navigationHistory = navigationHistory.filter((h:any) => {
-                
-                return h.filePath!==val.filePath && h.pattern!==val.pattern;
-                
+
+        vscode.window.showQuickPick(navigationHistory).then(val => {
+            navigationHistory = navigationHistory.filter((h: any) => {
+
+                return h.filePath !== val.filePath && h.pattern !== val.pattern;
+
             });
         });
-  
+
     });
 
     context.subscriptions.push(disposableFindTags);
@@ -67,20 +96,20 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposableClearOneNavigationHistory);
 }
 
-function loadTags(tagFilePath){
-    let tags=[];  
+function loadTags(tagFilePath) {
+    let tags = [];
     let liner = new LineByLine(tagFilePath);
     let line;
     let lineNumber = 0;
     while (line = liner.next()) {
         let elements = line.toString('ascii').split("\t");
         let tagName, fileName;
-        let remainingElements = elements.filter((el, index)=>{
-            if(index === 0) {
+        let remainingElements = elements.filter((el, index) => {
+            if (index === 0) {
                 tagName = el;
                 return false;
             }
-            if(index === 1) {
+            if (index === 1) {
                 fileName = el;
                 return false;
             }
@@ -96,7 +125,7 @@ function loadTags(tagFilePath){
             description: "",
             label: tagName,
             detail: fileName,
-            filePath: path.join(vscode.workspace.rootPath,fileName),
+            filePath: path.join(vscode.workspace.rootPath, fileName),
             pattern: patternEscaped
         });
         lineNumber++;
@@ -107,63 +136,63 @@ function loadTags(tagFilePath){
 function searchTags(context: vscode.ExtensionContext, tags: Array<Tags>) {
     let editor = getEditor();
     let query = getSelectedText(editor);
-    
+
     let displayFiles = tags.filter((tag, index) => {
         return tag.label === query;
     });
-            
+
     //Case 1. Only one tag found  
-    if(displayFiles.length === 1){
+    if (displayFiles.length === 1) {
         recordHistory(displayFiles[0]);
-        saveWorkspaceState(context, STATE_KEY, { navigationHistory : navigationHistory });        
-        navigateToDefinition(displayFiles[0].filePath,displayFiles[0].pattern);
-    //Case 2. Many tags found
-    }else if(displayFiles.length > 0){
+        saveWorkspaceState(context, STATE_KEY, {navigationHistory: navigationHistory});
+        navigateToDefinition(displayFiles[0].filePath, displayFiles[0].pattern);
+        //Case 2. Many tags found
+    } else if (displayFiles.length > 0) {
         vscode.window.showQuickPick(displayFiles, {matchOnDescription: true, matchOnDetail: true}).then(val => {
-            recordHistory(val);
-            saveWorkspaceState(context,STATE_KEY,{navigationHistory:navigationHistory});  
-            navigateToDefinition(val.filePath,val.pattern);
-        }
-    );
-    //Case 3. No tags found    
-    }else{
-        vscode.window.showInformationMessage('No related tags are found for the "'+query+'"');
+                recordHistory(val);
+                saveWorkspaceState(context, STATE_KEY, {navigationHistory: navigationHistory});
+                navigateToDefinition(val.filePath, val.pattern);
+            }
+        );
+        //Case 3. No tags found
+    } else {
+        vscode.window.showInformationMessage('No related tags are found for the "' + query + '"');
     }
-  
+
 }
 
-function recordHistory(visistedFile:any) {
+function recordHistory(visistedFile: any) {
     let isRecorded = false;
-    if(navigationHistory.length < 20){
+    if (navigationHistory.length < 20) {
         navigationHistory.map(val => {
             //if the filePath was already in the Histroy, we will ignore it.
-            if( val.filePath ===  visistedFile.filePath && val.pattern === visistedFile.pattern){
+            if (val.filePath === visistedFile.filePath && val.pattern === visistedFile.pattern) {
                 isRecorded = true;
             }
         });
-        if(!isRecorded){
+        if (!isRecorded) {
             navigationHistory.push(visistedFile);
-        }        
-    }else{
+        }
+    } else {
         navigationHistory.splice(1);
         navigationHistory.push(visistedFile);
     }
 
     //save to the session
     let savedState = {
-        navigationHistory:navigationHistory
+        navigationHistory: navigationHistory
     }
-    
+
 }
 
-function navigateToDefinition(filePath:string,pattern:string) {
-    vscode.workspace.openTextDocument(filePath).then( d => {
+function navigateToDefinition(filePath: string, pattern: string) {
+    vscode.workspace.openTextDocument(filePath).then(d => {
         vscode.window.showTextDocument(d).then(textEditor => {
-            fileGrep(fs.createReadStream(d.fileName), pattern)                     
+            fileGrep(fs.createReadStream(d.fileName), pattern)
                 .on('found', function (term, lineNumber) {
-                    goTolLine(lineNumber);                                
-                });                  
-        });                       
+                    goTolLine(lineNumber);
+                });
+        });
     });
 }
 
@@ -186,18 +215,18 @@ function getSelectedText(editor: vscode.TextEditor) {
 }
 
 function goTolLine(line: number) {
-    line = line===0 ? line : line-1;
+    line = line === 0 ? line : line - 1;
     let newSelection = new vscode.Selection(line, 0, line, 0);
-    vscode.window.activeTextEditor.selection = newSelection;      
+    vscode.window.activeTextEditor.selection = newSelection;
     vscode.window.activeTextEditor.revealRange(newSelection, vscode.TextEditorRevealType.InCenter);
 }
 
-function saveWorkspaceState(context : vscode.ExtensionContext, key: string,value:any): void {     
+function saveWorkspaceState(context: vscode.ExtensionContext, key: string, value: any): void {
     context.workspaceState.update(key, JSON.stringify(value));
 }
 
-function restoreWorkspaceState(context : vscode.ExtensionContext, key: string, callback:Function): void {     
-    callback(context.workspaceState.get(key,''));
+function restoreWorkspaceState(context: vscode.ExtensionContext, key: string, callback: Function): void {
+    callback(context.workspaceState.get(key, ''));
 }
 
 // this method is called when your extension is deactivated
@@ -205,8 +234,8 @@ export function deactivate() {
 }
 
 interface Tags {
-    description: string, 
-    label: string, 
+    description: string,
+    label: string,
     detail: string,
     filePath: string,
     pattern: string
